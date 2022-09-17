e0a3f64cfe759cae0cce0c68fe1ca4e98fdd0fa8
EbichanOO <takanori.sao@icloud.com>
2022-09-17 19:23:23 +0900

    [fix] hello function change HTML:hello to String:hello

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "Hello, World!"
 
 @app.route('/init/object', methods=['GET'])
 def init_object():
